f73b2f4d443ba0f827afbd3298d4789a19cf0178
Kerry Shetline <kerry@shetline.com>
2022-11-06 14:06:17 -0500

    Improve ZeroFinder constructor. (#10)

--- src/zero-finder.spec.ts
+++ src/zero-finder.spec.ts
@@ -5,7 +5,9 @@ const epsilon = 1E-10;
 
 describe('ZeroFinder', () => {
   it('should correctly find the zero of a function', () => {
-    const zf = new ZeroFinder(Math.sin, epsilon / 100, 50, 0.2, Math.sin(0.2), 3, Math.sin(3));
+    let zf = new ZeroFinder(Math.sin, epsilon / 100, 50, 0.2, Math.sin(0.2), 3, Math.sin(3));
+    expect(Math.abs(zf.getXAtZero() - Math.PI)).to.be.lessThan(epsilon);
+    zf = new ZeroFinder(Math.sin, epsilon / 100, 50, 0.2, 3);
     expect(Math.abs(zf.getXAtZero() - Math.PI)).to.be.lessThan(epsilon);
   });
 });
