9d3c86e89731702b024406c54e2f0e9b4a8e821e
Kerry Shetline <kerry@shetline.com>
2025-06-27 22:26:45 -0400

    Improved accuracy of ZeroFinder.

--- src/zero-finder.spec.ts
+++ src/zero-finder.spec.ts
@@ -3,24 +3,34 @@ import { ZeroFinder } from './zero-finder';
 import { HALF_PI } from './math';
 
 const epsilon = 1E-10;
+const tolerance = epsilon / 100;
 
 describe('ZeroFinder', () => {
   it('should correctly find the zero of a function', () => {
-    let zf = new ZeroFinder(Math.sin, epsilon / 100, 50, 0.2, Math.sin(0.2), 3, Math.sin(3));
+    let zf = new ZeroFinder(Math.sin, tolerance, 50, 0.2, Math.sin(0.2), 3, Math.sin(3));
 
     expect(Math.abs(zf.getXAtZero() - Math.PI)).to.be.lessThan(epsilon);
     expect(zf.lastY).to.approximately(0, epsilon);
     expect(zf.iterationCount).to.be.greaterThan(5);
+    expect(zf.resolved).to.be.true;
 
-    zf = new ZeroFinder(Math.cos, epsilon / 100, 50, 0.2, 3);
+    zf = new ZeroFinder(Math.cos, tolerance, 50, 0.2, 3);
 
+    expect(Math.abs(zf.getXAtZero() - HALF_PI)).to.be.lessThan(epsilon);
     expect(Math.abs(zf.getXAtZero() - HALF_PI)).to.be.lessThan(epsilon);
 
     // Range includes a potential trouble spot, which seems to be the only way to exercise
     //   a portion of the code.
-    zf = new ZeroFinder(Math.tan, epsilon / 100, 50, -1.5, 1.7);
+    zf = new ZeroFinder(Math.tan, tolerance, 50, -1.5, 1.7);
 
     zf.getXAtZero();
     expect(zf.lastY).to.approximately(0, epsilon);
+
+    const zff = () => new ZeroFinder(x => x ** 3, tolerance, 50, -3, 2);
+
+    expect(zff().getXAtZero()).to.approximately(0, epsilon);
+    expect(zff().lastY).to.approximately(0, epsilon);
+    expect(zff().iterationCount).to.equal(39);
+    expect(zff().resolved).to.be.true;
   });
 });
--- src/zero-finder.ts
+++ src/zero-finder.ts
@@ -1,10 +1,14 @@
 // noinspection JSSuspiciousNameCombination
 
+const INTERVALS = 10;
+
 export class ZeroFinder {
   private _iterationCount = 0;
   private x2: number;
   private y: number;
   private y2: number;
+  private _resolved = false;
+  private _x: number;
 
   constructor(zeroSeekingFunction: (x: number) => number, tolerance: number, maxIterations: number,
     x1: number, x2: number);
@@ -26,31 +30,100 @@ export class ZeroFinder {
   }
 
   getXAtZero(): number {
+    if (this._x != null)
+      return this._x;
+
     let x: number;
+    let maxIter = this.maxIterations / 2;
+    let triedIntervals = false;
 
     this._iterationCount = 0;
 
-    while (++this._iterationCount <= this.maxIterations) {
-      x = this.x1 - this.y1 / (this.y2 - this.y1) * (this.x2 - this.x1);
-      this.y = this.zeroSeekingFunction(x);
+    while (!this._resolved) {
+      while (++this._iterationCount <= maxIter && !this._resolved) {
+        x = this.x1 - this.y1 / (this.y2 - this.y1) * (this.x2 - this.x1);
+        this.y = this.zeroSeekingFunction(x);
 
-      if (Math.abs(this.y) <= this.tolerance)
+        if (Math.abs(this.y) <= this.tolerance) {
+          this._resolved = Math.abs(this.x2 - this.x1) <= this.tolerance;
+          this._x = x;
+          break;
+        }
+
+        if ((this.y1 < this.y2 && this.y < 0) || (this.y1 > this.y2 && this.y > 0)) {
+          this.x1 = x;
+          this.y1 = this.y;
+        }
+        else {
+          this.x2 = x;
+          this.y2 = this.y;
+        }
+      }
+
+      if (!this._resolved && !triedIntervals) {
+        triedIntervals = true;
+        maxIter = this.maxIterations;
+
+        outer:
+        while (++this._iterationCount <= maxIter) {
+          const step = (this.x2 - this.x1) / INTERVALS;
+          let i = 1;
+          x = this.x1;
+          let y = this.zeroSeekingFunction(x);
+          let sign = Math.sign(y);
+
+          do {
+            if (Math.abs(y) <= this.tolerance && step <= this.tolerance) {
+              this._resolved = true;
+              this.y = y;
+              break outer;
+            }
+
+            x += step;
+            y = this.zeroSeekingFunction(x);
+            const sign2 = Math.sign(y);
+
+            if (sign2 !== sign) {
+              this.x1 = x - step;
+              this.x2 = x;
+              x = x - step / 2;
+              break;
+            }
+          } while (++i <= INTERVALS);
+
+          if (i > INTERVALS)
+            break;
+        }
+
+        this.y = this.zeroSeekingFunction(x); // A last approximation in case iterations are exhausted
+      }
+      else
         break;
-
-      if ((this.y1 < this.y2 && this.y < 0) || (this.y1 > this.y2 && this.y > 0)) {
-        this.x1 = x;
-        this.y1 = this.y;
-      }
-      else {
-        this.x2 = x;
-        this.y2 = this.y;
-      }
     }
+
+    this._x = x;
 
     return x;
   }
 
-  get lastY(): number { return this.y; }
+  get lastY(): number {
+    if (this._x == null)
+      this.getXAtZero();
 
-  get iterationCount(): number { return this._iterationCount; }
+    return this.y;
+  }
+
+  get iterationCount(): number {
+    if (this._x == null)
+      this.getXAtZero();
+
+    return this._iterationCount;
+  }
+
+  get resolved(): boolean {
+    if (this._x == null)
+      this.getXAtZero();
+
+    return this._resolved && isFinite(this.y) && !isNaN(this.y);
+  }
 }
