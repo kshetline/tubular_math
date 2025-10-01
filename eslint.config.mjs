c9f98b72c68fa2c3b7ff9646ac90257ca516fd07
Kerry Shetline <kerry@shetline.com>
2025-10-01 16:57:45 -0400

    Fixed a package.json problem with Angular/Nx (#17)
    * Update ESLint.
    * Fixed a package.json problem with Angular/Nx.
    * Make sure SphericalPosition3D.xyz is immutable.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -47,6 +47,7 @@ export default [
         }
       ],
       '@stylistic/curly': 'off',
+      '@/eqeqeq': ['error', 'smart'],
       '@typescript-eslint/explicit-member-accessibility': [
         'error',
         {
